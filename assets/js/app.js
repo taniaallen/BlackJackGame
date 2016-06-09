fd657a7c206b082132902792d4ec2754283e5c63
Tania A <taniaaallen@gmail.com>
2016-06-09 10:23:37 -0400

    changed the image backgroundand updated alignmnent in CSS

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
 	console.log('loaded');
 
+	var $bet = $('#bet');
+	console.log($bet);
 	
+	$bet.on('click', betClicked);
 })
+
+
+function betClicked() {
+	console.log('bet button clicked');
+}
