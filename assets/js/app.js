a5356d6de02a25ae6fb4066165842badec846ade
Tania A <taniaaallen@gmail.com>
2016-06-10 11:40:01 -0400

    updated file names of club cards and have function for grabbing key to generate card

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,8 +12,22 @@ $(document).ready(function() {
 
 // Array of the card strings
 
-var deck = ['cardClubs2.png', 'cardClubs3.png', 'cardClubs4.png', 'cardClubs5.png', 'cardClubs6.png', 'cardClubs7.png', 'cardClubs8.png', 'cardClubs9.png', 'cardClubs10.png', 'cardClubsA.png', 'cardClubsJ.png', 'cardClubsK.png', 'cardClubsQ.png', 'cardDiamonds2.png', 'cardDiamonds3.png', 'cardDiamonds4.png', 'cardDiamonds5.png', 'cardDiamonds6.png', 'cardDiamonds7.png', 'cardDiamonds8.png', 'cardDiamonds9.png', 'cardDiamonds10.png', 'cardDiamondsA.png', 'cardDiamondsJ.png', 'cardDiamondsK.png', 'cardDiamondsQ.png'];
+var deck = ['clubs2.png', 'clubs3.png', 'cardClubs4.png', 'cardClubs5.png', 'cardClubs6.png', 'cardClubs7.png', 'cardClubs8.png', 'cardClubs9.png', 'clubs10.png', 'cardClubsA.png', 'cardClubsJ.png', 'cardClubsK.png', 'cardClubsQ.png', 'cardDiamonds2.png', 'cardDiamonds3.png', 'cardDiamonds4.png', 'cardDiamonds5.png', 'cardDiamonds6.png', 'cardDiamonds7.png', 'cardDiamonds8.png', 'cardDiamonds9.png', 'cardDiamonds10.png', 'cardDiamondsA.png', 'cardDiamondsJ.png', 'cardDiamondsK.png', 'cardDiamondsQ.png'];
 
+var deckObject = {
+	'clubs2.png': 2,
+	'clubs3.png': 3,
+	'clubs4.png': 4,
+	'clubs5.png': 5,
+	'clubs6.png': 6,
+	'clubs7.png': 7,
+	'clubs8.png': 8,
+	'clubs9.png': 9,
+	'clubs10.png': 10,
+	'clubsJ.png': 10,
+	'clubsK.png': 10,
+	'clubsQ.png': 10
+}
 
 
 // Variables for the pot, betting and player balance
@@ -36,6 +50,7 @@ function betClicked() {
 		balanceNumber -= 10;
 		// 10.00 should be added to the winning pot
 		potBalance += 10;
+		console.log($balance);
 		// update the text of balance to reflect the new sum of balanceNumber
 		$balance.text(balanceNumber);
 		// update the text of pot to reflect the new sum of potBalance
@@ -59,26 +74,39 @@ function dealCards() {  // function to determine how many cards to deal and who 
 	// player one gets a face up card
 	var playerCard = getCard();
 
-	if(playerCard === faceDown) {
-		playerCard = getCard();
-	};
-		generatePlayerCards(playerCard);
-		// console.log('player card: ' + playerCard);
 	
-	// dealer gets a face up card
-	var faceUp = getCard();
-	if(faceUp === faceDown || faceUp === playerCard) {
-		faceUp = getCard();
-	};
-		generateDealerCards(faceUp);
-		// console.log('face up: ' + faceUp);
+
+
+
+
+
+
+
+
+
+
+
+
+	// if(playerCard === faceDown) {  This particular set up didn't account for when the same card was randomly chosen twice in a row for this spot
+	// 	playerCard = getCard();
+	// };
+	// 	generatePlayerCards(playerCard);
+	// 	// console.log('player card: ' + playerCard);
 	
-	// player one gets last face up card
-	var playerCard2 = getCard();
-	if(playerCard2 === faceDown || playerCard2 === faceUp || playerCard2 === playerCard) {
-		playerCard2 = getCard();
-	};
-		generatePlayerCards(playerCard2);
+	// // dealer gets a face up card
+	// var faceUp = getCard();
+	// if(faceUp === faceDown || faceUp === playerCard) {
+	// 	faceUp = getCard();
+	// };
+	// 	generateDealerCards(faceUp);
+	// 	// console.log('face up: ' + faceUp);
+	
+	// // player one gets last face up card
+	// var playerCard2 = getCard();
+	// if(playerCard2 === faceDown || playerCard2 === faceUp || playerCard2 === playerCard) {
+	// 	playerCard2 = getCard();
+	// };
+	// 	generatePlayerCards(playerCard2);
 		// console.log('player Card 2: ' + playerCard2)
 	// $('button').off('#bet');  attempted to prevent the button from being clicked but syntax may not be right
 	
@@ -91,13 +119,19 @@ var $hit = $('#hit');  // if the player clicks the hit button, generate a new ca
 $hit.on('click', hitClicked); 
 
 
-function getCard() {    // function to create and return a card when invoked
+
+
+function getCard(value) {    // function to create and return a card when invoked
 	// Variable for random numbering
-	var randomNumber =  Math.floor(Math.random() * 26);
-	// pop item out of array?
-	//return random card
-	var card = deck[randomNumber];
-	return card;
+	var randomNumber =  Math.floor((Math.random() * 11) + 1);
+	// find key by value in an object
+
+	for(var key in deckObject) {
+		if(deckObject[key] === randomNumber) {            // Found this for loop on Stack Overflow: http://stackoverflow.com/questions/9907419/javascript-object-get-key-by-value
+			var card = key;
+			
+		}
+	}
 }
 
 function generateDealerCards(card) {  // Create a function that generates the image tag for the card 
@@ -116,7 +150,11 @@ function hitClicked() {
 	// console.log(hitCard);
 	generatePlayerCards(hitCard);
 
+	
+
 }
+
+
 
 
 
